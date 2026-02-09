7492a9303396a2747a90f5fb438b921cd9cadb32
Witalij <witalij.rapicki@gmail.com>
2026-02-09 18:10:47 +0100

    f:

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -3,46 +3,10 @@ resource "aws_api_gateway_rest_api" "contact_api" {
   name        = "contact-form-api"
   description = "API do formularza kontaktowego z załącznikiem"
 
+  # Włącz CORS na poziomie API (zalecane)
   endpoint_configuration {
     types = ["REGIONAL"]
   }
-
-  # Opcjonalnie: globalny CORS (jeśli chcesz mieć na całym API)
-  # body = jsonencode({
-  #   swagger = "2.0"
-  #   info = {
-  #     title = "Contact API"
-  #     version = "1.0"
-  #   }
-  #   paths = {
-  #     "/contact" = {
-  #       post = {
-  #         responses = {
-  #           "200" = {
-  #             description = "200 response"
-  #             headers = {
-  #               "Access-Control-Allow-Origin" = { type = "string" }
-  #             }
-  #           }
-  #         }
-  #       }
-  #       options = {
-  #         consumes = ["application/json"]
-  #         produces = ["application/json"]
-  #         responses = {
-  #           "200" = {
-  #             description = "200 response"
-  #             headers = {
-  #               "Access-Control-Allow-Origin" = { type = "string" }
-  #               "Access-Control-Allow-Methods" = { type = "string" }
-  #               "Access-Control-Allow-Headers" = { type = "string" }
-  #             }
-  #           }
-  #         }
-  #       }
-  #     }
-  #   }
-  # })
 }
 
 # Resource /contact
@@ -60,7 +24,7 @@ resource "aws_api_gateway_method" "contact_post" {
   authorization = "NONE"
 }
 
-# Integracja z Lambdą (AWS_PROXY)
+# Integracja z Lambdą (AWS_PROXY – najprostsza i najwydajniejsza)
 resource "aws_api_gateway_integration" "contact_lambda" {
   rest_api_id             = aws_api_gateway_rest_api.contact_api.id
   resource_id             = aws_api_gateway_resource.contact.id
@@ -70,40 +34,16 @@ resource "aws_api_gateway_integration" "contact_lambda" {
   uri                     = aws_lambda_function.contact_form.invoke_arn
 }
 
-# CORS dla POST – nagłówki odpowiedzi
-resource "aws_api_gateway_method_response" "contact_post_200" {
-  rest_api_id = aws_api_gateway_rest_api.contact_api.id
-  resource_id = aws_api_gateway_resource.contact.id
-  http_method = aws_api_gateway_method.contact_post.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
-    "method.response.header.Access-Control-Allow-Origin"  = true
-  }
+# Uprawnienia dla API Gateway do wywoływania Lambdy
+resource "aws_lambda_permission" "api_gateway_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.contact_form.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.contact_api.execution_arn}/*/*"
 }
 
-resource "aws_api_gateway_integration_response" "contact_post_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.contact_api.id
-  resource_id = aws_api_gateway_resource.contact.id
-  http_method = aws_api_gateway_method.contact_post.http_method
-  status_code = aws_api_gateway_method_response.contact_post_200.status_code
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'"
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-  }
-
-  response_templates = {
-    "application/json" = ""
-  }
-
-  depends_on = [aws_api_gateway_integration.contact_lambda]
-}
-
-# Metoda OPTIONS /contact – preflight
+# Metoda OPTIONS /contact – obsługa preflight (CORS)
 resource "aws_api_gateway_method" "contact_options" {
   rest_api_id   = aws_api_gateway_rest_api.contact_api.id
   resource_id   = aws_api_gateway_resource.contact.id
@@ -133,6 +73,10 @@ resource "aws_api_gateway_method_response" "contact_options_200" {
     "method.response.header.Access-Control-Allow-Methods" = true
     "method.response.header.Access-Control-Allow-Origin"  = true
   }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
 }
 
 resource "aws_api_gateway_integration_response" "contact_options_integration_response" {
@@ -152,10 +96,11 @@ resource "aws_api_gateway_integration_response" "contact_options_integration_res
   }
 }
 
-# Deployment
+# Deployment API Gateway
 resource "aws_api_gateway_deployment" "contact_deployment" {
   rest_api_id = aws_api_gateway_rest_api.contact_api.id
 
+  # Triggers – redeploy przy każdej zmianie w API
   triggers = {
     redeployment = sha1(jsonencode([
       aws_api_gateway_resource.contact.id,
@@ -163,8 +108,6 @@ resource "aws_api_gateway_deployment" "contact_deployment" {
       aws_api_gateway_integration.contact_lambda.id,
       aws_api_gateway_method.contact_options.id,
       aws_api_gateway_integration.contact_options_integration.id,
-      aws_api_gateway_method_response.contact_post_200.id,
-      aws_api_gateway_integration_response.contact_post_integration_response.id,
     ]))
   }
 
